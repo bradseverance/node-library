d405cb48e1705feaa7223b63fe00639d3428e187
Brad Severance <me@bradseverance.com>
2016-06-22 14:51:18 -0400

    stuff and things

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+// set up express
 var express = require('express');
-
 var app = express();
 
+// port
 var port = process.env.PORT || 5000;
+
+// nav
+var nav = [
+  {
+    link: '/Books',
+    text: 'Books'
+  },
+  {
+    link: '/Authors',
+    text: 'Authors'
+  }
+];
+
+
+// book router
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+// admin router
+var adminRouter = require('./src/routes/adminRoutes')(nav);
+
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-
 app.set('view engine', 'ejs');
+
+// use our routes
+app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
+
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Hello from render',
-    list: ['a', 'b']
+    title: 'Library',
+    nav: nav
   });
 });
 
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,57 @@
+var express = require('express');
+
+var bookRouter = express.Router();
+
+// mongo db
+var mongodb = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
+
+var router = function (nav) {
+
+  // book list
+  bookRouter.route('/').get(function (req, res) {
+
+    var url = 'mongodb://localhost:27017/libraryApp';
+
+    mongodb.connect(url, function (err, db) {
+      var collection = db.collection('books');
+      collection.find().toArray(function (err, results) {
+        res.render('bookListView', {
+          title: 'Book List',
+          nav: nav,
+          books: results
+        });
+        db.close();
+      });
+    });
+
+  });
+
+  // single book
+  bookRouter.route('/:id').get(function (req, res) {
+
+    var id = new objectID(req.params.id);
+
+    var url = 'mongodb://localhost:27017/libraryApp';
+
+    mongodb.connect(url, function (err, db) {
+      var collection = db.collection('books');
+      collection.findOne({ _id : id }, function (err, results) {
+        res.render('bookView', {
+          title: 'Book' + id,
+          nav: nav,
+          book: results
+        });
+        db.close();
+      });
+    });
+
+    
+
+  });
+
+  return bookRouter;
+
+}
+
+module.exports = router;
