4c655ac17466cd15edae140ce9efee69440930c5
Brad Severance <me@bradseverance.com>
2016-06-27 12:41:21 -0400

    stuff

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -9,14 +9,6 @@ var router = function (nav) {
 
   adminRouter.use(adminController.middleware);
 
-  // don't let anyone into the admin that's not me (basically)
-  adminRouter.use(function (req, res, next) {
-    if (!req.user) {
-      return res.redirect('/evilPerson');
-    }
-    next();
-  });
-
   adminRouter.route('/home').get(adminController.getHome);
 
   return adminRouter;
