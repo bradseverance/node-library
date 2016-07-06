44cff445694a50c05f927fd65b20c0739dd1a4f2
Brad P. Severance <me@bradseverance.com>
2016-07-06 12:16:04 -0400

    starting book edit

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,7 +2,7 @@
 var mongodb = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 
-var adminController = function (bookService, nav) {
+var adminController = function (goodReadsService, nav) {
 
   var middleware = function (req, res, next) {
     if (!req.user) {
@@ -29,8 +29,27 @@ var adminController = function (bookService, nav) {
 
   };
 
+  var getBook = function (req, res) {
+
+    var id = new ObjectID(req.params.id);
+
+    mongodb.connect(process.env.DB_URL, function (err, db) {
+      var collection = db.collection('books');
+      collection.findOne({_id : id}, function (err, results) {
+        res.render('adminBookEdit', {
+          title: 'Book' + id,
+          nav: nav,
+          book: results
+        });
+        db.close();
+      });
+    });
+
+  };
+
   return {
     getHome: getHome,
+    getBook: getBook,
     middleware: middleware
   };
 
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,13 +4,19 @@ var adminRouter = express.Router();
 
 var router = function (nav) {
 
+  // goodreads service
+   var goodReadsService = require('../services/goodReadsService')();
+
   // controller
-  var adminController = require('../controllers/adminController')(null, nav);
+  var adminController = require('../controllers/adminController')(goodReadsService, nav);
 
   //adminRouter.use(adminController.middleware);
 
   // home
   adminRouter.route('/home').get(adminController.getHome);
+
+  // edit book
+  adminRouter.route('/books/:id').get(adminController.getBook);
 
   return adminRouter;
 
